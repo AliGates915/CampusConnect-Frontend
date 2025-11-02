d7cd6aa12a9252cf81e89ad8d35021abaadf0983
Ali Gates <128673394+AliGates915@users.noreply.github.com>
2025-11-02 12:04:12 +0500

    Update Terms.jsx

--- src/pages/Terms.jsx
+++ src/pages/Terms.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import { faqs } from "../assets/assets";
+
+import { motion } from "framer-motion";
 import { SlideLeft, SlideUp } from "../utils/Animation";
 
 const Terms = () => {
